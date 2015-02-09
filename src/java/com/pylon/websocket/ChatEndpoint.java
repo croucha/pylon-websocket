4ff197af62a1d5bf04ca0aba32c0afeda894f3fd
Andre Crouch <crouch.andre@gmail.com>
2015-02-08 23:25:40 -0600

    updated formatting

--- src/java/com/pylon/websocket/ChatEndpoint.java
+++ src/java/com/pylon/websocket/ChatEndpoint.java
@@ -31,24 +31,23 @@ public class ChatEndpoint {
 	@OnOpen
 	public void open(final Session session, @PathParam("room") final String room) {
         log.trace("Session openend and bound to room: {}", room);
-		session.getUserProperties().put("room", room);
+        session.getUserProperties().put("room", room);
 	}
  
-	@OnMessage
-	public void onMessage(final Session session, final ChatMessage chatMessage) {
+    @OnMessage
+    public void onMessage(final Session session, final ChatMessage chatMessage) {
 		String room = (String) session.getUserProperties().get("room");
-        log.info("Endpoint message: {}", chatMessage);
+        log.trace("Endpoint message: {}", chatMessage);
 		try {
-			for (Session s : session.getOpenSessions()) {
-				if (s.isOpen()
-						&& room.equals(s.getUserProperties().get("room"))) {
-					s.getBasicRemote().sendObject(chatMessage);
-				}
-			}
-		} catch (IOException | EncodeException e) {
+            for (Session s : session.getOpenSessions()) {
+                if (s.isOpen() && room.equals(s.getUserProperties().get("room"))) {
+                    s.getBasicRemote().sendObject(chatMessage);
+                }
+            }
+        } catch (IOException | EncodeException e) {
             log.trace("onMessage failed: {}", e);
-		}
-	}
+        }
+    }
 
     @OnError
     public void onError(Throwable t) {
