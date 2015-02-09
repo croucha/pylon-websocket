7a4948758d58ce0e6bf323bf050fb26e3ab10b52
Andre Crouch <crouch.andre@gmail.com>
2015-02-08 23:26:30 -0600

    updated formatting

--- src/java/com/pylon/websocket/ChatEndpoint.java
+++ src/java/com/pylon/websocket/ChatEndpoint.java
@@ -28,15 +28,15 @@ public class ChatEndpoint {
     
 	private final Logger log = LogManager.getLogger(getClass().getName()); 
  
-	@OnOpen
-	public void open(final Session session, @PathParam("room") final String room) {
+    @OnOpen
+    public void open(final Session session, @PathParam("room") final String room) {
         log.trace("Session openend and bound to room: {}", room);
         session.getUserProperties().put("room", room);
-	}
+    }
  
     @OnMessage
     public void onMessage(final Session session, final ChatMessage chatMessage) {
-		String room = (String) session.getUserProperties().get("room");
+        String room = (String) session.getUserProperties().get("room");
         log.trace("Endpoint message: {}", chatMessage);
 		try {
             for (Session s : session.getOpenSessions()) {
