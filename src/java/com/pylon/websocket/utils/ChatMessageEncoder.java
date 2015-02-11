a79d36dd884e76c72c4433b544383e244f090fcd
Andre Crouch <crouch.andre@gmail.com>
2015-02-11 06:58:44 -0600

    convert received to ISO 8601 format

--- src/java/com/pylon/websocket/utils/ChatMessageEncoder.java
+++ src/java/com/pylon/websocket/utils/ChatMessageEncoder.java
@@ -1,8 +1,12 @@
 package com.pylon.websocket.utils;
 
 import com.google.gson.Gson;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.TimeZone;
 import javax.websocket.EncodeException;
 import javax.websocket.Encoder;
 import javax.websocket.EndpointConfig;
@@ -32,7 +36,15 @@ public class ChatMessageEncoder
         Map<String,String> results = new LinkedHashMap<>();
         results.put("message", chatMessage.getMessage());
         results.put("sender", chatMessage.getSender());
-        results.put("received", chatMessage.getReceived().toString());
+        // Define time zone
+        TimeZone timeZone = TimeZone.getTimeZone("UTC");
+        // Define Atom (ISO 8601) format
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssz");
+        // Set time zone
+        dateFormat.setTimeZone(timeZone);
+        // Covert to ISO 8601
+        String iso8601 = dateFormat.format(chatMessage.getReceived());
+        results.put("received", iso8601);
         return new Gson().toJson(results);
     }
 }
